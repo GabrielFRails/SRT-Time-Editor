a0afa8aeea0fd775f06d091ddb7fb19aa635e476
GabrielFRails <gabriel.estudy.reis@gmail.com>
2025-01-04 19:48:07 -0300

    add argc and argv to main run

--- main.cpp
+++ main.cpp
@@ -1,14 +1,38 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <unistd.h>
 
 #define STR_TIME_PATTERN "-->"
 #define ERROR_FILE_OPEN "Was not possible open the srt file"
+#define ERROR_RIGHT_USE "Usage: -f <caminho_do_srt> -t <milissegundos> -o <delay|advance>"
 
 using namespace std;
 
-int main() {
-	ifstream srtFile("./Industry.S01E01.srt");
+int main(int argc, char *argv[]) {
+	string filePath;
+	int timeShift = 0;
+	string operation;
+
+	int opt;
+	while((opt = getopt(argc, argv, "f:t:o:")) != -1) {
+		switch (opt) {
+			case 'f':
+				filePath = optarg;
+				break;
+			case 't':
+				timeShift = atoi(optarg);
+				break;
+			case 'o':
+				operation = optarg;
+				break;
+			default:
+				cerr << ERROR_RIGHT_USE << endl;
+				return 1;
+		}
+	}
+
+	ifstream srtFile(filePath);
 	if (!srtFile.is_open()) {
 		cerr << ERROR_FILE_OPEN << endl;
 		return 1;
